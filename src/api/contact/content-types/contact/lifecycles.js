f2c9095b15cee643275ae991bf048ebe4659fc2c
Wahyu Saefulloh <saefuuloh@gmail.com>
2025-03-03 10:03:52 +0700

    revamp

--- src/api/contact/content-types/contact/lifecycles.js
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -3,14 +3,14 @@ const transporter = require('../../../../helpers/email-service');
 module.exports = {
     async afterCreate(event) {
         console.log('masuk')
-        await transporter.sendMail({
-            from: {
-                name: "Katarsis Notification Email",
-                address: process.env.EMAIL_ADDRESS_FROM,// sender address
-            },
-            to: process.env.EMAIL_ADDRESS_FOR,// list of receivers
-            subject: 'Get in Touch',// Subject line
-            html: `<div>Nama : ${event.params.data.name} <br/> Email : ${event.params.data.email} <br/> Pesan : ${event.params.data.message}</div>`
-        });
+        // await transporter.sendMail({
+        //     from: {
+        //         name: "Katarsis Notification Email",
+        //         address: process.env.EMAIL_ADDRESS_FROM,// sender address
+        //     },
+        //     to: process.env.EMAIL_ADDRESS_FOR,// list of receivers
+        //     subject: 'Get in Touch',// Subject line
+        //     html: `<div>Nama : ${event.params.data.name} <br/> Email : ${event.params.data.email} <br/> Pesan : ${event.params.data.message}</div>`
+        // });
     },
 };
